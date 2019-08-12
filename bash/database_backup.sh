9f8cc5c093e91c5b6f2ef3b710b5ac0ccf2ee5ae
4815162342lost <family-gan@yandex.ru>
2019-08-12 14:48:06 +0300

    Update database_backup.sh

--- bash/database_backup.sh
+++ bash/database_backup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+#script for make backup of the postgres database
+#criterias: make backup every day except weekends
+#make backup on the first day of the mounth and year and store permanently
+#remove backup automatically older than retention_period variable
+
+#database name
+database_name="bars_web_bb"
 
 #backup stored dir
 daily_backup_dir="/home/backup/backup_db/daily/"
 monthly_backup_dir="/home/backup/backup_db/monthly/"
 yearly_backup_dir="/home/backup/backup_db/yearly/"
 
-#save the current dates to variables
+#rsync ip addr and dir
+rsync_command_args="root@10.1.4.72:/root/backup/bars_web_bb.backup"
+
+#save the current dates to variables (Jan 1 just for tests and debug purposes)
 #day_of_month=`date +%d  -d 'Jan 1 13:34:04 MSK 2019'`
 #day_of_year=`date +%j  -d 'Jan 1 13:34:04 MSK 2019'`
 #day_of_week=`date +%u -d 'Jan 1 13:34:04 MSK 2019'`
@@ -41,34 +51,36 @@ echo "weekends variable: $weekends"
 if [ "$weekends" != "yes" ]
   then
     echo "Starting backup creation..."
-    sudo -E -u postgres pg_dump -U web_bb -h 127.0.0.1 -Fc -f $daily_backup_dir
+    sudo -E -u postgres pg_dump -U web_bb -h 127.0.0.1 -Fc -f $daily_backup_dir$database_name$file_name $database_name
+    #make rsync
+    rsync-zvh $daily_backup_dir$database_name$file_name $rsync_command_args
 fi
 
 #make backup if first day of month and weekends, or just copy if not weekends
 if [ "$day_of_month" -eq "1" ]
   then
-    echo 'copy first day of month'
+    echo 'first day of month is true'
   if [ "$weekends" != "yes" ]
     then
       echo "Copying backup to $monthly_backup_dir"
-      cp $daily_backup_dir$file_name $monthly_backup_dir
+      cp $daily_backup_dir$database_name$file_name $monthly_backup_dir
   else
     echo "Starting backup creation..."
-    sudo -E -u postgres pg_dump -U web_bb -h 127.0.0.1 -Fc -f $monthly_backup_dir
+    sudo -E -u postgres pg_dump -U web_bb -h 127.0.0.1 -Fc -f $monthly_backup_dir$database_name$file_name $database_name
   fi
 fi
 
 #make backup if first day of year and weekends, or just copy if not weekends
 if [ "$day_of_year" == "001" ]
   then
-    echo "first day of the year"
+    echo "first day of the year is true"
     if [ "$weekends" != "yes" ]
       then
       echo "Copying backup to $yearly_backup_dir"
-      cp $daily_backup_dir$file_name $yearly_backup_dir
+      cp $daily_backup_dir$database_name$file_name $yearly_backup_dir
     else
       echo "Starting backup creation..."
-      sudo -E -u postgres pg_dump -U web_bb -h 127.0.0.1 -Fc -f $yearly_backup_dir
+      sudo -E -u postgres pg_dump -U web_bb -h 127.0.0.1 -Fc -f $yearly_backup_dir$database_name$file_name $database_name
    fi
 fi
 
