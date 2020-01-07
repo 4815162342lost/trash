2ec5c0e114ba221307dd0b191e408cbbbc1c8457
4815162342lost <family-gan@yandex.ru>
2020-01-07 23:22:43 +0300

    Update database_backup.sh

--- bash/database_backup.sh
+++ bash/database_backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 ###script for make backup of the postgres database
 ###criterias: make backup every day except weekends
-###make backup on the first day of the mounth and year and store permanently
+###make backup on Jan 1 and store permanently
 ###remove backup automatically older than retention_period variable
 ###store monthly backup in 366 days
 
-#database name
+#database connection params
 database_name="bars_web_bb"
+database_user="web_bb"
+datapase_host="127.0.0.1"
+database_port=5432
 
 #backup stored dir
 daily_backup_dir="/home/backup/backup_db/daily/"
@@ -14,28 +17,25 @@ monthly_backup_dir="/home/backup/backup_db/monthly/"
 yearly_backup_dir="/home/backup/backup_db/yearly/"
 
 #rsync ip addr and dir
-rsync_command_args="root@10.1.4.72:/root/backup/bars_web_bb.backup"
+#rsync_command_args="root@10.1.4.72:/root/backup/bars_web_bb.backup"
 
-#save the current dates to variables (Jan 1 just for tests and debug purposes)
-#day_of_month=`date +%d  -d 'Jan 1 13:34:04 MSK 2019'`
-#day_of_year=`date +%j  -d 'Jan 1 13:34:04 MSK 2019'`
-#day_of_week=`date +%u -d 'Jan 1 13:34:04 MSK 2019'`
+#get current date
 day_of_month=`date +%d`
 day_of_year=`date +%j`
 day_of_week=`date +%u`
 
 
-#keep backups 7 days and geberate backup name
+#keep backups 7 days and generate backup name
 retention_period=7
 file_name=`date +%d_%m_%Y.dump`
 
 #determine is now weekends or not
-if [ "$day_of_week" -eq "6" ] || [ "$day_of_week" -eq "7" ]
+if [ "$day_of_week" -eq "1" ] || [ "$day_of_week" -eq "7" ]
   then
-    echo 'weekends, not make backup'
+    echo 'were weekends, do not make on Sunday or Monday (bacuse we are running script at 03:00 am)'
     weekends="yes"
     #if Suterday then retention day + 1, if Sunday +2
-    if [ "$day_of_week" -eq "6" ] 
+    if [ "$day_of_week" -eq "7" ] 
       then
         retention_period=$(( $retention_period + 1 ))
     else
@@ -52,9 +52,9 @@ echo "weekends variable: $weekends"
 if [ "$weekends" != "yes" ]
   then
     echo "Starting backup creation..."
-    sudo -E -u postgres pg_dump -U web_bb -h 127.0.0.1 -Fc -f $daily_backup_dir$database_name$file_name $database_name
+    sudo -E -u postgres pg_dump -p $database_port  -U $database_user -h $datapase_host -Fc -f $daily_backup_dir$database_name$file_name -d $database_name
     #make rsync
-    rsync -zvh $daily_backup_dir$database_name$file_name $rsync_command_args
+    #rsync -zvh $daily_backup_dir$database_name$file_name $rsync_command_args
 fi
 
 #make backup if first day of month and weekends, or just copy if not weekends
